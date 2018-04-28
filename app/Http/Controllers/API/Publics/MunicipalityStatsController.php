a6a7d0f242a26d680703cd860e11e2383fbb1f35
Mohamed Chams Eddin Mouedhen <chams-med@hotmail.fr>
2018-04-28 03:55:20 +0100

    add public stats api

--- app/Http/Controllers/API/Publics/MunicipalityStatsController.php
+++ app/Http/Controllers/API/Publics/MunicipalityStatsController.php
@@ -12,11 +12,12 @@ class MunicipalityStatsController extends Controller
     public function __invoke($id)
     {
         $stat = DB::table('theme_translations')
-            ->select(DB::raw('theme_translations.theme_id AS id, theme_translations.name as theme, color, COUNT(*) as count'))
+            ->select(DB::raw('theme_translations.theme_id AS id, theme_translations.name as theme, municipality_id, color, COUNT(*) as count'))
             ->join('themes', 'themes.id', '=', 'theme_translations.theme_id')
             ->join('complains', 'complains.theme_id', '=', 'theme_translations.theme_id')
             ->where('locale', '=', App::getLocale())
-            ->groupBy(['theme', 'id', 'color'])
+            ->where('municipality_id', '=', $id)
+            ->groupBy(['theme', 'id', 'color', 'municipality_id'])
             ->get();
 
         return response()->json($stat);
