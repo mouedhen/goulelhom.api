b9c738179e7e7c40c2b0d66924d2a2200138c418
Mohamed Chams Eddin Mouedhen <chams-med@hotmail.fr>
2018-05-21 11:56:36 +0100

    change petitions resource

--- app/Http/Resources/Petitions/PetitionResource.php
+++ app/Http/Resources/Petitions/PetitionResource.php
@@ -2,18 +2,53 @@
 
 namespace App\Http\Resources\Petitions;
 
+use App\Http\Resources\Contacts\ContactResource;
+use App\Http\Resources\Helpers\MediaResource;
+use App\Http\Resources\Stacked\MunicipalityStackedResource;
+use App\Http\Resources\Stacked\SignatureStackedResource;
+use App\Http\Resources\Stacked\ThemeStackedResource;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\Resource;
 
 class PetitionResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'uuid' => $this->uuid,
+            'description' => $this->description,
+            'requested_signatures_number' => $this->requested_signatures_number,
+            'status' => $this->status,
+
+            'is_new' => $this->is_new === 1,
+            'is_moderated' => $this->is_moderated === 1,
+            'is_valid' => $this->is_valid === 1,
+            'has_approved_sworn_statement' => $this->has_approved_sworn_statement === 1,
+            'has_approved_term_of_use' => $this->has_approved_term_of_use === 1,
+            'is_boosted' => $this->is_boosted === 1,
+
+            'created_at' => (string)$this->created_at,
+            'start_date' => (string)$this->start_date,
+            'end_date' => (string)$this->end_date,
+
+            'attachments' => MediaResource::collection($this->media),
+
+            'contact' => new ContactResource($this->contact),
+            'theme' => new ThemeStackedResource($this->theme),
+            'organization' => new Resource($this->organization),
+
+            'wasArchived' => $this->wasArchived(),
+            'haveReachedObjective' => $this->haveReachedObjective(),
+            'signatures' => $this->signatures->count(),
+            'signatories' => SignatureStackedResource::collection($this->signatures),
+        ];
     }
 }
